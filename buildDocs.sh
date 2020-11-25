a3b2746f521fe850d155dd0923f73db51e060dc3
probonopd <probonopd@users.noreply.github.com>
2020-11-25 20:19:12 +0100

    rsync -av "./_build/" "${docroot}/"

--- buildDocs.sh
+++ buildDocs.sh
@@ -48,7 +48,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
  
 docroot=`mktemp -d`
-rsync -av "./_build/html/" "${docroot}/"
+rsync -av "./_build/" "${docroot}/"
  
 pushd "${docroot}"
  
