ade661a74a1979f8e4855c2b2528b1bfeffad773
probonopd <probonopd@users.noreply.github.com>
2020-11-25 19:18:50 +0100

    sudo pip3 install commonmark

--- buildDocs.sh
+++ buildDocs.sh
@@ -18,7 +18,8 @@ set -x
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-recommonmark
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-pip
+sudo pip3 install commonmark
  
 #####################
 # DECLARE VARIABLES #
