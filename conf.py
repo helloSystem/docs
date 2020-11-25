7d305f1ac25da31d8dbf95865e51d1c63fea6fc1
probonopd <probonopd@users.noreply.github.com>
2020-11-25 23:04:21 +0100

    Extra '.md' added

--- conf.py
+++ conf.py
@@ -74,9 +74,10 @@ html_context = {
 html_static_path = ['_static']
 
 github_doc_root = 'https://github.com/' + os.environ['GITHUB_REPOSITORY'] + '/tree/main/'
+# FIXME: Extra '.md' added
 def setup(app):
     app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
+            'url_resolver': lambda url: github_doc_root + url + '.md',
             'auto_toc_tree_section': 'Contents',
             }, True)
     app.add_transform(AutoStructify)
